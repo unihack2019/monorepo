6b89896dedaac0dfba0c04ee500f75afe61acd47
Joseph Kim <joseph.kim.sungha@gmail.com>
2019-03-16 21:53:51 +1100

    Include repo scope in OAuth request

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,9 +5,10 @@ export default class Home extends Component {
   render() {
     const clientId = "43fd6cb45d67a0b47ef4";
     const redirectUrl = "http://localhost:3000/github";
+    const scope = "repo";
     return (
       <div>
-        <Button href={`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}`}>Authorize Github</Button>
+        <Button href={`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope}`}>Authorize Github</Button>
       </div>
     );
   }
