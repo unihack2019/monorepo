554af52b864e41b11e451a11355d490535442e22
sudhirmandarapu <sudhirmandarapu@gmail.com>
2019-03-17 02:03:18 +1100

    Set repositories in analyse

---
+++ backend/src/analyse.js
@@ -0,0 +1,35 @@
+const database = require("./api/firebase");
+const Octokit = require("@octokit/rest");
+
+function analyse(githubId, token) {
+  // set_technologies(githubId, token);
+  set_repositories(githubId, token);
+}
+
+async function set_technologies(githubId, token) {
+  // todo: do stuff
+  const octokit = new Octokit({auth: `token ${token}`});
+  technologies = [];
+  database.collection("applicants").doc(githubId).set({technologies});
+}
+
+async function set_repositories(githubId, token) {
+  const octokit = new Octokit({auth: `token ${token}`});
+  const reposResponse = await octokit.repos.list({per_page: 1});
+  const repos = reposResponse.data;
+
+  const repositories = await Promise.all(repos.map(async (githubRepo) => {
+    const [owner, repo] = githubRepo.full_name.split("/");
+    const languagesResponse = await octokit.repos.listLanguages({owner, repo});
+    const languages = languagesResponse.data;
+    const technologies = Object.keys(languages).map(name => ({
+      name,
+      locs: languages[name]
+    }));
+
+    return {githubRepo, technologies}
+  }));
+  await database.collection("applicants").doc(githubId).update({repositories});
+}
+
+module.exports = analyse;
