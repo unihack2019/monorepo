4a65430f0f75282d5dd35f2aaed5395b85d86c3e
Patrick Shaw <psha67@student.monash.edu>
2019-03-16 14:21:00 +1100

    Added a initial JobListPage

--- frontend/src/JobListPage.js
+++ frontend/src/JobListPage.js
@@ -1,4 +1,34 @@
 import React from 'react';
-const JobListPage = () => (
-  <div>Hi Erfan and Joseph</div>
+
+const JobTechnology = ({ technology }) => (
+  <div>{technology.name}</div>
+);
+
+const JobItem = ({ job }) => (
+  <section>
+    <h1>{job.title}</h1>
+    <p>{job.description}</p>
+    <div>{
+      job.technologies.map(technology => (
+        <JobTechnology technology={technology}/>
+      ))
+    }</div>
+  </section>
+);
+
+const jobs = [
+  {
+    title: 'Bleh',
+    description: 'Swek',
+    technologies: ['Freaking awesome pants swekness lol']
+  },
+  {
+    title: 'OMG so swek',
+    description: 'rawr',
+    technologies: ['Hi david']
+  }
+];
+
+export const JobListPage = () => (
+  jobs.map(job => <JobItem job={job}/>)
 );
