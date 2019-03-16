cd57d2c664e7f7b1ce717b4a876253e9660ec239
eastrd <yjia285@student.monash.edu>
2019-03-17 02:42:49 +1100

    Add the forked repo filter & Fix bad fork bugs

--- backend/src/analyse.js
+++ backend/src/analyse.js
@@ -8,28 +8,29 @@ function analyse(githubId, token) {
 
 async function set_technologies(githubId, token) {
   // todo: do stuff
-  const octokit = new Octokit({auth: `token ${token}`});
+  const octokit = new Octokit({ auth: `token ${token}` });
   technologies = [];
-  database.collection("applicants").doc(githubId).set({technologies});
+  database.collection("applicants").doc(githubId).set({ technologies });
 }
 
 async function set_repositories(githubId, token) {
-  const octokit = new Octokit({auth: `token ${token}`});
-  const reposResponse = await octokit.repos.list({per_page: 1});
-  const repos = reposResponse.data;
+  const octokit = new Octokit({ auth: `token ${token}` });
+  const reposResponse = await octokit.repos.list({ per_page: 100 });
+
+  const repos = reposResponse.data.filter(repo => !repo.fork);
 
   const repositories = await Promise.all(repos.map(async (githubRepo) => {
     const [owner, repo] = githubRepo.full_name.split("/");
-    const languagesResponse = await octokit.repos.listLanguages({owner, repo});
+    const languagesResponse = await octokit.repos.listLanguages({ owner, repo });
     const languages = languagesResponse.data;
     const technologies = Object.keys(languages).map(name => ({
       name,
       locs: languages[name]
     }));
 
-    return {githubRepo, technologies}
+    return { githubRepo, technologies }
   }));
-  await database.collection("applicants").doc(githubId).update({repositories});
+  await database.collection("applicants").doc(githubId).update({ repositories });
 }
 
 module.exports = analyse;
