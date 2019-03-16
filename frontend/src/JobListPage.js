cb0c76603a4d63bfc7ad664bb4ee5929a3f8c426
Patrick Shaw <psha67@student.monash.edu>
2019-03-16 17:27:04 +1100

    More fixes

--- frontend/src/JobListPage.js
+++ frontend/src/JobListPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typography, withStyles } from '@material-ui/core';
 import JobTechnology from './TechnologyChip';
 import RepositorySection from './RepositorySection';
 
