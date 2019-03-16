743df509db6d8433aed5d3e84587aa68988b0326
Patrick Shaw <psha67@student.monash.edu>
2019-03-17 06:21:59 +1100

    tech relevancy methods

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,26 @@
 import { decorate, observable, action } from 'mobx';
 import db from './api/firebase';
 
+function divideSeries(n) {
+  return Math.log(Math.E, n) + 0.5772156649;
+}
+
 class Store {
   applicants = {};
+  jobs = {};
+  technologiesRelevant(githubId, jobId) {
+    const applicant = this.applicants[githubId];
+    const job = this.jobs[jobId];
+    return job.technologies.filter(jobTech =>
+      applicant.technologies.some(applicantTech => applicantTech.name === jobTech.name),
+    );
+  }
+  tehnologyScore(technologies) {
+    return (
+      technologies.reduce((total, tech, index) => total + tech.score / (index + 1), 0) /
+      divideSeries(technologies.length)
+    );
+  }
 }
 decorate(Store, {
   applicants: observable,
