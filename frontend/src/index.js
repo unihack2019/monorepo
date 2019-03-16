07a8b7e55ab05f45215cbf297855d53278867472
Joseph Kim <joseph.kim.sungha@gmail.com>
2019-03-16 15:04:05 +1100

    Added in Firebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
+import config from "./firebaseSecret";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Initialize Firebase
+firebase.initializeApp(config);
+
+ReactDOM.render(
+  <App
+    database={firebase.firestore()}
+  />,
+  document.getElementById('root')
+);
